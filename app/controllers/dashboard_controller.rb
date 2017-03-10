9628a62c1eac4e23f1d11b8aa22f2ccfa7a17d3a
shivakumaarmgs <shivakumaarmgs@gmail.com>
2017-03-10 23:41:10 +0530

    devise added

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,4 +3,8 @@ class DashboardController < ApplicationController
   def home
 
   end
+
+  def user_home
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+ 
+  authenticated :user do
+    root :to => 'dashboard#user_home', as: :authenticated_root
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'dashboard#home' 
 end
